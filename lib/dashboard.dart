c6d1c9fd9cac287aa71615b7b7962a92603a14e0
dennysayuwardani <dennysayuwardani@gmail.com>
2025-01-10 17:52:27 +0700

    Menambahkan navbar di homepage

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -8,15 +9,73 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _currentIndex = 0; // Menyimpan indeks tombol yang aktif
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index; // Memperbarui indeks aktif
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: Row(
+          children: [
+            const Icon(Icons.store_mall_directory_outlined),
+            Text('Kasir',
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )),
+          ],
+        ),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(onPressed: () {
+            //Aksi
+          }, icon: const Icon(Icons.notifications_active_outlined))
+        ],
       ),
-      body: const Center(
-        child: Text('Selamat datang di Dashboard!'),
+      body: Center(
+        child: Text(
+          _currentIndex == 0
+              ? 'Produk'
+              : _currentIndex == 1
+                  ? 'Transaksi'
+                  : 'Profil',
+          style: const TextStyle(fontSize: 24),
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex, // Indeks tombol yang aktif
+        selectedItemColor: Colors.blue, // Warna tombol aktif
+        unselectedItemColor: Colors.grey, // Warna tombol tidak aktif
+        onTap: _onTap, // Callback saat tombol diklik
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Produk',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Transaksi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Profil',
+          ),
+        ],
+        selectedLabelStyle: GoogleFonts.poppins(
+          fontSize: 14,
+          fontWeight: FontWeight.bold
+        ),
+        unselectedLabelStyle: GoogleFonts.poppins(
+          fontSize: 12,
+        ),
+      )
+    
     );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -20,218 +20,215 @@ class Login extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  bool _isLoading = false;
-  bool _isPasswordVisible = false;
+  bool _isPasswordVisible = false; // Untuk mengontrol visibilitas password
+  bool _isLoading = false; // Untuk mengontrol loading state
+
+  final SupabaseClient supabase = Supabase.instance.client;
 
   Future<void> _login() async {
     setState(() {
       _isLoading = true;
     });
 
-    final username = _usernameController.text.trim();
-    final password = _passwordController.text;
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
 
-    if (username.isEmpty || password.isEmpty) {
-      _showSnackBar('Username dan Password wajib diisi!');
+    try {
+      // Query ke Supabase untuk validasi user
+      final response = await supabase
+          .from('user')
+          .select()
+          .eq('username', username)
+          .eq('password', password)
+          .maybeSingle();
+
+      if (response != null) {
+        // Login berhasil
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const Dashboard()),
+        );
+      } else {
+        // Login gagal
+        _showSnackBar('Invalid username or password');
+      }
+    } catch (e) {
+      _showSnackBar('An error occurred: $e');
+    } finally {
       setState(() {
         _isLoading = false;
       });
-      return;
     }
+  }
 
-    try {
-    final response = await Supabase.instance.client.auth.signInWithPassword(
-      email: username,
-      password: password,
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
     );
-
-    // Memeriksa apakah login berhasil
-    if (response.user == null) {
-      // Jika pengguna null, berarti login gagal
-      _showSnackBar('Login gagal. Periksa username dan password Anda!');
-    } else {
-      // Login berhasil
-      _showSnackBar('Login berhasil!');
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => Dashboard()),
-      );
-    }
-  } catch (error) {
-    // Menangkap kesalahan dan menampilkan pesan yang sesuai
-    _showSnackBar('Error: ${error.toString()}');
-  } finally {
-    setState(() {
-      _isLoading = false;
-    });
-  }
   }
 
-    void _showSnackBar(String message) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
-      );
-    }
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
-    @override
-    void dispose() {
-      _usernameController.dispose();
-      _passwordController.dispose();
-      super.dispose();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          backgroundColor: Colors.white,
-          body: Center(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //Icon User
-                    const CircleAvatar(
-                      radius: 55,
-                      backgroundColor: Colors.blue,
-                      child: Icon(
-                        Icons.person,
-                        size: 85,
-                        color: Colors.white,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //Icon User
+                  const CircleAvatar(
+                    radius: 55,
+                    backgroundColor: Colors.blue,
+                    child: Icon(
+                      Icons.person,
+                      size: 85,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Selamat Datang",
+                    style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  // const SizedBox(height: 5),
+                  Text(
+                    "Silahkan Login",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
+                  const SizedBox(height: 45),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Username Field
+                      Text(
+                        "Username",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.blue,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      "Selamat Datang",
-                      style: GoogleFonts.poppins(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                    // const SizedBox(height: 5),
-                    Text(
-                      "Silahkan Login",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(height: 45),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        //Username Field
-                        Text(
-                          "Username",
-                          style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                  color: Colors.grey.shade300, width: 1),
-                            ),
-                          ),
-                          child: Row(
-                            children: [
-                              const Icon(Icons.person, color: Colors.blue),
-                              const SizedBox(width: 10),
-                              Expanded(
-                                child: TextField(
-                                controller: _usernameController,
-                                decoration: InputDecoration(
-                                  hintText: "Username",
-                                  hintStyle: GoogleFonts.poppins(
-                                    fontSize: 14,
-                                  ),
-                                  border: InputBorder.none,
-                                ),
-                              ))
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        //Password Field
-                        Text(
-                          "Password",
-                          style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                  color: Colors.grey.shade300, width: 1),
-                            ),
-                          ),
-                          child: Row(
-                            children: [
-                              const Icon(Icons.lock, color: Colors.blue),
-                              const SizedBox(width: 10),
-                              Expanded(
-                                child: TextField(
-                                controller: _passwordController,
-                                obscureText: !_isPasswordVisible,
-                                decoration: InputDecoration(
-                                  hintText: "Password",
-                                  hintStyle: GoogleFonts.poppins(
-                                    fontSize: 14,
-                                  ),
-                                  border: InputBorder.none,
-                                ),
-                              )),
-                              const Spacer(),
-                              IconButton(
-                                icon: Icon(
-                                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                                  color: Colors.grey,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _isPasswordVisible = !_isPasswordVisible; // Toggle visibilitas
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-
-                    ElevatedButton(
-                      onPressed: _isLoading ? null : _login,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
+                      Container(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 70, vertical: 15),
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                                color: Colors.grey.shade300, width: 1),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            const Icon(Icons.person, color: Colors.blue),
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: TextField(
+                              controller: _usernameController,
+                              decoration: InputDecoration(
+                                hintText: "Username",
+                                hintStyle: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                ),
+                                border: InputBorder.none,
+                              ),
+                            ))
+                          ],
+                        ),
                       ),
-                      child: _isLoading
-                        ? const CircularProgressIndicator(color: Colors.white)
-                        :Text("Login",
-                          style: GoogleFonts.poppins(
-                            fontSize: 20,
-                          )),
-                    )
-                  ]),
-            ),
-          ));
-    }
-  }
+                      const SizedBox(height: 10),
+                      //Password Field
+                      Text(
+                        "Password",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                                color: Colors.grey.shade300, width: 1),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            const Icon(Icons.lock, color: Colors.blue),
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: TextField(
+                              controller: _passwordController,
+                              obscureText: !_isPasswordVisible,
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                hintStyle: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                ),
+                                border: InputBorder.none,
+                              ),
+                            )),
+                            const Spacer(),
+                            IconButton(
+                              icon: Icon(
+                                _isPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.grey,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordVisible =
+                                      !_isPasswordVisible; // Toggle visibilitas
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
 
+                  ElevatedButton(
+                    onPressed: _isLoading ? null : _login,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 70, vertical: 15),
+                    ),
+                    child: _isLoading
+                        ? const CircularProgressIndicator(color: Colors.white)
+                        : Text("Login",
+                            style: GoogleFonts.poppins(
+                              fontSize: 20,
+                            )),
+                  )
+                ]),
+          ),
+        ));
+  }
+}
